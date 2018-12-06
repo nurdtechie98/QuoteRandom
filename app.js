fc48283048a17d0797042bea7d057fca8040e69a
nurdtechie98 <nurdtechie98@gmail.com>
2018-12-07 01:32:18 +0530

    add loader + title

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 async function fetchNew() {
+    document.getElementById("quote").style.display='none';
+    document.getElementById("author").style.display='none';
+    document.getElementById("loader").style.display='block';
     const quote = await fetch('https://talaikis.com/api/quotes/random/');
     const json = await quote.json();
-    //console.log(json.author);
     document.getElementById("quote").innerHTML="&#34; "+json.quote+" &#34;";
     document.getElementById("author").innerHTML="- "+json.author;
+    document.getElementById("loader").style.display='none';
+    document.getElementById("quote").style.display='block';
+    document.getElementById("author").style.display='block';
 }
 
 function reload(){
@@ -15,8 +20,8 @@ function random_bg_color() {
     var y = Math.floor(Math.random() * 150);
     var z = Math.floor(Math.random() * 150);
     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-    //console.log(bgColor);
     document.body.style.background = bgColor;
+    document.getElementById("title").style.color = bgColor;
     document.getElementsByClassName("btn")[0].style.background= bgColor;
 }
 
